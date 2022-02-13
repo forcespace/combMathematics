ed014dce0725c9eb16f8e88d6664a03490b90fa5
forcespace <force.space@rambler.ru>
2022-02-13 21:09:33 +0300

    1

--- labs/01/1.1/main.cpp
+++ labs/01/1.1/main.cpp
@@ -1,7 +1,26 @@
+#include <algorithm>
 #include <iostream>
+#include <vector>
+#include <boost/timer.hpp>
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    std::vector<size_t> v = {7, 1, 12, 5, 9, 20, 15, 2, 3, 4, 21, 23, 26, 28, 99};
+    boost::timer timer;
+    timer.restart();
+    std::sort(v.begin(), v.end());
+    do
+    {
+        copy(v.begin(), v.end(), std::ostream_iterator<size_t>(std::cout, " "));
+        std::cout << std::endl;
+    }
+    while (std::next_permutation(v.begin(), v.end()));
+    double duration = timer.elapsed();
+
+    std::cout << duration << std::endl;
 }
+
+/*
+ * 7 - 0.2s
+ * 10 - 17s
+ */
